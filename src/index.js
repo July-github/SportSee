61c70d4b194baee9a7cdf56214610d69f6d00ea4
Julie Heckler <july.heckler@gmail.com>
2022-05-05 11:55:04 +0200

    sass

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './style/scss/style.scss';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className='hello'>Hello !</div>
   </React.StrictMode>
 );
 
